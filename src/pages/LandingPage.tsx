9c6c7f031142fd07b1a569a4365ab427983b051a
João Campos <joaon.c.jv@gmail.com>
2025-09-15 22:05:22 -0300

    fix: duplicated footer on Landing Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,7 @@
-import { Footer } from "../components/layout/Footer"
-
 export const LandingPage = () => {
   return (
     <>
       <main><h1 className="text-blue-3">landing page</h1></main>
-      <Footer />
     </>
   )
 }
